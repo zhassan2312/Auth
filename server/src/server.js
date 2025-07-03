39a013788beeeed1e9d6707dbd655d1bc9458ea0
Zohaib Hassan <zhassan.ce44ceme@student.nust.edu.pk>
2025-07-03 15:10:59 +0500

    Add user authentication middleware
    Introduced a new middleware to handle user authentication for protected routes. This ensures only authenticated users can access certain endpoints.

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser'
 
 
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
 
 // Sample route
 app.use('/', (req, res) => {
