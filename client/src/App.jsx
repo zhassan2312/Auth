6e55bd9fddd4b912c80a865e7c0ac679c8b0ce15
Zohaib Hassan <zhassan.ce44ceme@student.nust.edu.pk>
2025-07-04 11:11:26 +0500

    Add routing and basic page components
    Introduced react-router-dom and set up routing structure in the app. Added placeholder components for HomePage, LoginPage, SignupPage, and VerificationPage. Updated dependencies to include axios, zustand, react-toastify, and react-router-dom.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
-
+import {Route,Routes} from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import VerificationPage from './pages/Verificationpage'
 function App() {
   
   return (
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {RouterProvider} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </StrictMode>,
 )
